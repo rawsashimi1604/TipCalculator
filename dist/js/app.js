335f78fb41cb933507ddbfe77f83af6f7fd8bbcf
Gavin Loo <looweiren@gmail.com>
2021-09-03 23:43:19 +0800

    changed Bill html, made period editable

--- dist/js/app.js
+++ dist/js/app.js
@@ -17,7 +17,7 @@ const resetButton = document.querySelector("#reset");
 function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode === 190) {
         return false;
     }
     return true;
